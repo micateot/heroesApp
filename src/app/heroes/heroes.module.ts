8afeb950a69ad7ecfd3edf86c0835ebfd1864b7c
MicaTeot <micaelat@itcsoluciones.com>
2024-05-22 11:23:56 -0300

    Styling heroes layout page

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -19,6 +20,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
+
     HeroesRoutingModule,
   ]
 })
