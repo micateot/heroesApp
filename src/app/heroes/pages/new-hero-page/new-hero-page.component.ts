aee971b9d4f15ce44fce110030665ef084ff923b
MicaTeot <micaelat@itcsoluciones.com>
2024-05-24 17:27:24 -0300

    Adding and updating hero

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-hero-page',
@@ -8,8 +11,52 @@ import { Component } from '@angular/core';
 })
 export class NewHeroPageComponent {
 
-  public publisers = [
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.MarvelComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  })
+
+  public publishers = [
     {id: 'DC Comics', description: 'DC - Comics'},
     {id: 'Marvel Comics', description: 'Marvel - Comics'}
   ]
+
+  constructor(private heroesService: HeroesService){}
+
+
+  public get currentHero() : Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+
+
+
+  onSubmit():void{
+    // console.log({
+    //   formIsValid: this.heroForm.valid,
+    //   value: this.heroForm.value,
+    //   rawValue: this.heroForm.getRawValue()
+    // });
+
+    if(this.heroForm.invalid) return;
+
+    if(this.currentHero.id){
+      this.heroesService.updateHero( this.currentHero )
+        .subscribe( hero => {
+          // TODO Mostrar snackbar
+        });
+      return;
+    }
+
+    this.heroesService.addHero(this.currentHero)
+      .subscribe( hero => {
+        // TODO Mostrar snackbar y navegar a /heroes/edit/heroID
+      })
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -24,6 +24,23 @@ export class HeroesService {
 
   getSuggestions(query: string):Observable<Hero[]>{
     return this.httpClient.get<Hero[]>(`${this.baseURL}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero>{
+    return this.httpClient.post<Hero>(`${this.baseURL}/heroes/`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero>{
+    if(!hero.id) throw Error('El id es obligatorio');
+    return this.httpClient.patch<Hero>(`${this.baseURL}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHero(heroId: string): Observable<boolean>{
+    return this.httpClient.delete(`${this.baseURL}/heroes/${heroId}`)
+      .pipe(
+        catchError( error => of(false)),
+        map( response => true)
+      )
   }
 
 }
