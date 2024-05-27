0d1fa1d8c7c21c0c2af55c4bd6381469799c767a
MicaTeot <micaelat@itcsoluciones.com>
2024-05-27 13:09:24 -0300

    WIP Checking authentication

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 
 @Injectable({providedIn: 'root'})
@@ -32,5 +32,18 @@ export class AuthService {
     localStorage.clear();
   }
 
+  public checkAuthentication(): Observable<boolean> {
+    if(!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseURL}/users/1`).pipe(
+      tap(user => this.user = user),
+      map(user => !!user),
+      tap(value => console.log({value})),
+      catchError(err => of(false))
+    )
+
+  }
 
 }
